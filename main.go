854dcfa6df5953a3f88888492b7c8a6b71db5127
Alex Guerra <alex9627@gmail.com>
2017-05-14 17:28:30 -0300

    Add version flag

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/aguerra/grp/server"
 	"github.com/kelseyhightower/envconfig"
 )
 
+var version string
+var showVersion bool
+
+func init() {
+	flag.BoolVar(&showVersion, "v", false, "Show version")
+	flag.Parse()
+}
+
 func main() {
+	if showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 	log.SetFlags(log.Lshortfile | log.LstdFlags | log.LUTC)
 	var conf server.ServerConfig
 	if err := envconfig.Process("grp", &conf); err != nil {
